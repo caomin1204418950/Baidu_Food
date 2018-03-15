c8ef71b91e7f8e63d0328e7a07893a9417d6166d
unknown <1204418950@qq.com>
2018-03-16 01:33:52 +0800

    finishedtabChange

--- src/components/xiaoer.vue
+++ src/components/xiaoer.vue
@@ -4,36 +4,81 @@
           小二上菜
       </div>
       <div class="xieoer_servies">
-          <div class="xiaoer_servies_item">点菜</div>
-          <div class="xiaoer_servies_item">评价</div>
-          <div class="xiaoer_servies_item">商家</div>
+            <div class="xiaoer_servies_item">
+                    <div @click="order" class="item_text">点菜</div>
+                    <div @click="judge" class="item_text">评价</div>
+                    <div @click="seller"class="item_text">商家</div>
+            </div>
+
+            <div class="servies_menu">
+                <div class="ordermenu"v-show="isorder">这是菜单</div> 
+                <div class="judge_menu" v-show="isjudge">这是评论</div>
+                <div class="seller_menu" v-show="isseller">这是商家信息</div>
+            </div>
       </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    data(){
+        return{
+            isorder:true,
+            isjudge:false,
+            isseller:false
+        }
+    },
+    methods:{
+        order:function(){
+            if(this.isorder===false){
+                this.isorder=true;
+                this.isjudge=false;
+                this.isseller=false
+            }
+        },
+        judge:function(){
+            console.log('评论')
+            if(this.isjudge===false){
+            this.isorder=false;
+            this.isjudge=true;
+            this.isseller=false
+            }
+           
+        },
+        seller:function(){
+            console.log('商家');
+            if(this.isseller===false){
+                this.isseller=true;
+                this.isorder=false;
+                this.isjudge=false
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
 .xieoer_servies{
     width: 100%;
-    height: 60px;
+    height: 200px;
     background-color: pink;
-    display:flex;
-    flex-direction: row;
-    justify-content: start;
-    align-items: center
 }
 .xiaoer_servies_item{
-    width: 20%;
-    height: 40px;
     margin-left: 20px;
+    background-color: blueviolet;
+      display:flex;
+    flex-direction: row;
+}
+.item_text{
+    width: 80px;
     background-color: greenyellow;
-    color:#333333;
     font-size: 20px;
+    
+}
+.servies_menu{
+    width: 100%;
+    height: 150px;
+    background-color: blue;
 }
 
 </style>
