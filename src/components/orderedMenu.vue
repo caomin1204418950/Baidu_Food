e9add0511d8afaa0497133dbfb966fc29aba90f5
unknown <1204418950@qq.com>
2018-03-17 23:49:29 +0800

    start the component of orderedMenu

---
+++ src/components/orderedMenu.vue
@@ -0,0 +1,94 @@
+<template>
+   <div class="container">
+        <div class="menuList">
+            主食
+        </div>
+         <div class="menuList">
+            主食
+        </div>
+         <div class="menuList">
+            主食
+        </div>
+         <div class="menuList">
+            主食
+        </div>
+         <div class="menuList">
+            食
+        </div>
+         <div class="menuList">
+            食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            主食
+        </div>
+          <div class="menuList">
+            食
+        </div>
+          <div class="menuList">
+            食
+        </div>
+          <div class="menuList">
+            食
+        </div>
+          <div class="menuList">
+            食
+        </div>
+          <div class="menuList">
+            食
+        </div>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .container{
+        width: 90px;
+        height: 80vh;
+        background-color: blue;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        overflow: hidden;
+    }
+    .menuList{
+        width: 100%;
+        height: 50px;
+        text-align: center;
+        line-height: 50px;
+    }
+</style>
--- src/components/xiaoer.vue
+++ src/components/xiaoer.vue
@@ -5,13 +5,13 @@
       </div>
       <div class="xieoer_servies">
             <div class="xiaoer_servies_item">
-                    <div @click="order" class="item_text">点菜</div>
-                    <div @click="judge" class="item_text">评价</div>
-                    <div @click="seller"class="item_text">商家</div>
+                    <div @click="order" class="item_text" :class="{active:changecolor==0}">点菜</div>
+                    <div @click="judge" class="item_text"  :class="{active:changecolor==1}">评价</div>
+                    <div @click="seller"class="item_text" :class="{active:changecolor==2}">商家</div>
             </div>
 
             <div class="servies_menu">
-                <div class="ordermenu"v-show="isorder">这是菜单</div> 
+                <div class="ordermenu"v-show="isorder"><orderedMenu></orderedMenu></div> 
                 <div class="judge_menu" v-show="isjudge">这是评论</div>
                 <div class="seller_menu" v-show="isseller">这是商家信息</div>
             </div>
@@ -20,37 +20,47 @@
 </template>
 
 <script>
+import orderedMenu from '@/components/orderedMenu'
 export default {
+    components:{
+        orderedMenu
+    },
     data(){
         return{
             isorder:true,
             isjudge:false,
-            isseller:false
+            isseller:false,
+            changecolor:0
         }
+
     },
     methods:{
-        order:function(){
+        order:function(e){
             if(this.isorder===false){
                 this.isorder=true;
                 this.isjudge=false;
-                this.isseller=false
+                this.isseller=false;
+                this.changecolor=0
             }
         },
-        judge:function(){
+        judge:function(e){
             console.log('评论')
             if(this.isjudge===false){
             this.isorder=false;
             this.isjudge=true;
-            this.isseller=false
+            this.isseller=false;
+            this.changecolor=1
             }
            
         },
-        seller:function(){
+        seller:function(e){
             console.log('商家');
             if(this.isseller===false){
                 this.isseller=true;
                 this.isorder=false;
-                this.isjudge=false
+                this.isjudge=false;
+                this.changecolor=2
+                
             }
         }
     }
@@ -61,24 +71,36 @@ export default {
 .xieoer_servies{
     width: 100%;
     height: 200px;
-    background-color: pink;
 }
 .xiaoer_servies_item{
-    margin-left: 20px;
-    background-color: blueviolet;
       display:flex;
     flex-direction: row;
 }
 .item_text{
-    width: 80px;
-    background-color: greenyellow;
+    width: 50px;
+    height: 40px;
     font-size: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 20px;
     
+    
+}
+.active{
+    color: red;
+    border-bottom:  3px  solid #FF2D4B;
+     width: 50px;
+    font-size: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 20px;
 }
 .servies_menu{
     width: 100%;
-    height: 150px;
-    background-color: blue;
+    height: 80vh;
+    background-color: pink;
 }
 
 </style>
